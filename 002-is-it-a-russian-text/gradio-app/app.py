7cd375b86dcb2d3b08d8d8824261e0b94709f355
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-04-18 14:01:29 -0300

    002: refactor gradio app

--- 002-is-it-a-russian-text/gradio-app/app.py
+++ 002-is-it-a-russian-text/gradio-app/app.py
@@ -8,7 +8,10 @@ import gradio as gr
 learn = load_learner("export.pkl")
 
 categories = ("Not Russian text", "Russian text")
+
+base_path = "examples/"
 examples = ["es-wikipedia.png", "pt-wikipedia.png", "ru-rt.png", "ru-wikipedia.png"]
+examples = [base_path + filename for filename in examples]
 
 def classify_image(img):
     pred,idx,probs = learn.predict(img)
